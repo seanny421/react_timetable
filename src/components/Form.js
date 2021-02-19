2880f7e82f26d653917c54646610ea75c42058ad
Sean McGeachie <sean.mcgeachie.2017@uni.strath.ac.uk>
2021-02-19 21:53:24 +0000

    Basic template up, added side navigation component

---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+function Form(){
+
+
+    return(
+        <form>
+            <input type="text" className="todo-input"/>
+            <button className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">Today</option>
+                    <option value="monday">Monday</option>
+                    <option value="tuesday">Tuesday</option>
+                    <option value="wednesday">Wednesday</option>
+                    <option value="thursday">Thursday</option>
+                    <option value="friday">Friday</option>
+                    <option value="saturday">Saturday</option>
+                    <option value="sunday">Sunday</option>
+                </select>
+            </div>
+        </form>
+
+    );
+};
+
+export default Form; 

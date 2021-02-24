f7ede7da1384c9035d731e86345f212dc99c70ab
Sean McGeachie <sean.mcgeachie.2017@uni.strath.ac.uk>
2021-02-24 01:23:38 +0000

    added animation when deleting todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,12 @@
-import React from "react";
-
-
-
-
-
+import React, { useState } from "react";
 
 function Todo({todos, setTodos, todoText, todo}){
+    const [isDeleted, setIsDeleted] = useState(false);
 
     function deleteHandler(e){
-        setTodos(todos.filter(element => element.id !== todo.id));
+        setIsDeleted(true);
+        //use setTimeout so use can see the animation when fall class is added 
+        setTimeout(() => {setTodos(todos.filter(element => element.id !== todo.id));}, 500);
     };
     
     function completedHandler(){
@@ -16,8 +14,8 @@ function Todo({todos, setTodos, todoText, todo}){
     };
 
     return(
-        <div className={`todo`}>
-            <li className="todo-item">{todoText}</li>
+        <div className={`todo ${isDeleted ? "fall": ""}`}>
+            <li className={`todo-item`}>{todoText}</li>
             <button onClick={completedHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
